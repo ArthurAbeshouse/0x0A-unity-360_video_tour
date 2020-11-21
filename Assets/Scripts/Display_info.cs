8d9f146e63010321a523424f80e81522ffa11c8f
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-11-20 21:44:22 -0500

    Initital push

---
+++ Assets/Scripts/Display_info.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Display_info : MonoBehaviour
+{
+    private bool display = false;
+    public void Info()
+    {
+        if (display == false)
+        {
+            gameObject.SetActive(true);
+            display = true;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            display = false;
+        }
+    }
+}
---
+++ Assets/Scripts/VideoManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour
+{
+    private VideoPlayer vidplayer;
+
+    [SerializeField]
+    VideoClip[] vids;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidplayer = GetComponent<VideoPlayer>();
+    }
+    public void Play()
+    {
+        vidplayer.Play();
+    }
+    public void Pause()
+    {
+        vidplayer.Pause();
+    }
+    public void Stop()
+    {
+        vidplayer.Stop();
+    }
+
+    public void LivingSpace()
+    {
+        VidChange(0);
+    }
+
+    public void Cube()
+    {
+        VidChange(1);
+    }
+
+    public void Cantina()
+    {
+        VidChange(2);
+    }
+
+    public void Mezzanine()
+    {
+        VidChange(3);
+    }
+
+    public void VidChange(int vid_id)
+    {
+        vidplayer.source = VideoSource.VideoClip;
+        vidplayer.clip = vids[vid_id];
+        vidplayer.Prepare();
+        vidplayer.prepareCompleted += VideoPlayer_prep;
+    }
+
+    private void VideoPlayer_prep(VideoPlayer source)
+    {
+        Play();
+    }
+}
